1376b1a41275eac1b722c7272f79124e719fe35d
osegonte <segohopo@gmail.com>
2025-12-19 08:13:38 +0100

    0

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,6 +2,7 @@ from app.models.user import Organization, User
 from app.models.site import Site
 from app.models.worker import Worker
 from app.models.event import Device, ClockEvent
+from app.models.user_site import UserSite
 
 __all__ = [
     "Organization",
@@ -10,4 +11,5 @@ __all__ = [
     "Worker",
     "Device",
     "ClockEvent",
+    "UserSite",
 ]
